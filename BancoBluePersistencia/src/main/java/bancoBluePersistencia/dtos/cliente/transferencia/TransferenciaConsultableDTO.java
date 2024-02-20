cac70037681872e1ccb4b580bf70305367b95a5e
licoreeee <victoria.vega247804@potros.itson.edu.mx>
2024-02-19 19:14:18 -0700

    Documentación

--- BancoBluePersistencia/src/main/java/bancoBluePersistencia/dtos/cliente/transferencia/TransferenciaConsultableDTO.java
+++ BancoBluePersistencia/src/main/java/bancoBluePersistencia/dtos/cliente/transferencia/TransferenciaConsultableDTO.java
@@ -2,20 +2,37 @@
 package bancoBluePersistencia.dtos.cliente.transferencia;
 
 /**
- *
- * @author t1pas
+ * Clase que representa un DTO (Data Transfer Object) para consultar información de una transferencia.
+ * Contiene el código de la transferencia y métodos equals, hashCode y toString para facilitar la comparación y representación de objetos.
+ * 
+ * @author Jorge Zamora, Victoria Vega
  */
 public class TransferenciaConsultableDTO {
     private long codigo;
 
+    /**
+     * Método getter para obtener el código de la transferencia.
+     * 
+     * @return El código de la transferencia.
+     */
     public long getCodigo() {
         return codigo;
     }
 
+    /**
+     * Método setter para establecer el código de la transferencia.
+     * 
+     * @param codigo El nuevo código de la transferencia.
+     */
     public void setCodigo(long codigo) {
         this.codigo = codigo;
     }
 
+    /**
+     * Método hashCode para calcular el código hash del objeto.
+     * 
+     * @return El código hash del objeto.
+     */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -23,6 +40,12 @@ public class TransferenciaConsultableDTO {
         return hash;
     }
 
+    /**
+     * Método equals para comparar dos objetos.
+     * 
+     * @param obj El objeto a comparar.
+     * @return true si los objetos son iguales, false en caso contrario.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -38,6 +61,11 @@ public class TransferenciaConsultableDTO {
         return this.codigo == other.codigo;
     }
 
+    /**
+     * Método toString para representar el objeto como una cadena de texto.
+     * 
+     * @return La representación del objeto como cadena de texto.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- BancoBluePersistencia/src/main/java/bancoBluePersistencia/dtos/cliente/transferencia/TransferenciaNuevaDTO.java
+++ BancoBluePersistencia/src/main/java/bancoBluePersistencia/dtos/cliente/transferencia/TransferenciaNuevaDTO.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
- *
- * @author t1pas
+ * Clase que representa un DTO (Data Transfer Object) para la creación de una transferencia.
+ * Contiene todos los atributos necesarios para realizar una transferencia entre cuentas.
+ * Además, incluye métodos equals, hashCode y toString para facilitar la comparación y representación de objetos.
+ * 
+ * @author Jorge Zamora, Victoria Vega.
  */
 public class TransferenciaNuevaDTO {
     
@@ -17,48 +20,101 @@ public class TransferenciaNuevaDTO {
     
     private int numCuentaDestino;
 
+    /**
+     * Método getter para obtener el monto de la transferencia.
+     * 
+     * @return El monto de la transferencia.
+     */
     public long getMonto() {
         return monto;
     }
 
+    /**
+     * Método setter para establecer el monto de la transferencia.
+     * 
+     * @param monto El nuevo monto de la transferencia.
+     */
     public void setMonto(long monto) {
         this.monto = monto;
     }
 
+    /**
+     * Método getter para obtener el motivo de la transferencia.
+     * 
+     * @return El motivo de la transferencia.
+     */
     public String getMotivo() {
         return motivo;
     }
 
+    /**
+     * Método setter para establecer el motivo de la transferencia.
+     * 
+     * @param motivo El nuevo motivo de la transferencia.
+     */
     public void setMotivo(String motivo) {
         this.motivo = motivo;
     }
 
+    /**
+     * Método getter para obtener la fecha de creación de la transferencia.
+     * 
+     * @return La fecha de creación de la transferencia.
+     */
     public LocalDateTime getFechaCreacion() {
         return fechaCreacion;
     }
 
+    /**
+     * Método setter para establecer la fecha de creación de la transferencia.
+     * 
+     * @param fechaCreacion La nueva fecha de creación de la transferencia.
+     */
     public void setFechaCreacion(LocalDateTime fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
     }
 
+    /**
+     * Método getter para obtener el código de la cuenta origen de la transferencia.
+     * 
+     * @return El código de la cuenta origen de la transferencia.
+     */
     public long getCodigoCuenta() {
         return codigoCuenta;
     }
 
+    /**
+     * Método setter para establecer el código de la cuenta origen de la transferencia.
+     * 
+     * @param codigoCuenta El nuevo código de la cuenta origen de la transferencia.
+     */
     public void setCodigoCuenta(long codigoCuenta) {
         this.codigoCuenta = codigoCuenta;
     }
 
-    
-    
+    /**
+     * Método getter para obtener el número de cuenta destino de la transferencia.
+     * 
+     * @return El número de cuenta destino de la transferencia.
+     */
     public int getNumCuentaDestino() {
         return numCuentaDestino;
     }
 
+    /**
+     * Método setter para establecer el número de cuenta destino de la transferencia.
+     * 
+     * @param numCuentaDestino El nuevo número de cuenta destino de la transferencia.
+     */
     public void setNumCuentaDestino(int numCuentaDestino) {
         this.numCuentaDestino = numCuentaDestino;
     }
 
+    /**
+     * Método hashCode para calcular el código hash del objeto.
+     * 
+     * @return El código hash del objeto.
+     */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -70,6 +126,12 @@ public class TransferenciaNuevaDTO {
         return hash;
     }
 
+    /**
+     * Método equals para comparar dos objetos.
+     * 
+     * @param obj El objeto a comparar.
+     * @return true si los objetos son iguales, false en caso contrario.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -97,6 +159,11 @@ public class TransferenciaNuevaDTO {
         return Objects.equals(this.fechaCreacion, other.fechaCreacion);
     }
 
+    /**
+     * Método toString para representar el objeto como una cadena de texto.
+     * 
+     * @return La representación del objeto como cadena de texto.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
